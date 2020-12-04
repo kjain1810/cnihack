896223eb207d4ef2e1b8e3bf2f59b8f595c4b924
Arpan Dasgupta <arpandasgupta0@gmail.com>
2020-12-05 00:36:22 +0530

    Adding a simple greedy solution

---
+++ greedy/scorer.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double kmR = 6373.0;
+const int N = 502, M = 52;
+
+int num_districts = 0, num_labs = 0;
+
+struct Lab
+{
+    int id;
+    double lat, lon;
+    int district, type, capacity, backlog;
+} labz[N];
+
+struct District
+{
+    int id;
+    string name;
+    double lat, lon;
+    int samples;
+} districts[M];
+
+string get_parts(string &line, int &i)
+{
+    string word;
+    while (i < line.length())
+    {
+        if (line[i] == ',')
+        {
+            i++;
+            break;
+        }
+        else
+            word += line[i];
+        i++;
+    }
+    return word;
+}
+
+void get_lab(string &line, Lab &lab)
+{
+    int i = 0;
+    lab.id = stoi(get_parts(line, i));
+    lab.lat = stod(get_parts(line, i));
+    lab.lon = stod(get_parts(line, i));
+    lab.district = stoi(get_parts(line, i));
+    lab.type = stoi(get_parts(line, i));
+    lab.capacity = stoi(get_parts(line, i));
+    lab.backlog = stoi(get_parts(line, i));
+}
+
+void get_district(string &line, District &d)
+{
+    int i = 0;
+    d.id = stoi(get_parts(line, i));
+    d.name = get_parts(line, i);
+    d.lat = stod(get_parts(line, i));
+    d.lon = stod(get_parts(line, i));
+    d.samples = stoi(get_parts(line, i));
+}
+
+signed main()
+{
+
+    ifstream distin, labin;
+    distin.open("districts_data_v0.csv");
+    labin.open("lab_data_v0.csv");
+    string line;
+
+    getline(distin, line);
+    while (getline(distin, line))
+    {
+        get_district(line, districts[++num_districts]);
+    }
+
+    getline(labin, line);
+    while (getline(labin, line))
+    {
+        get_lab(line, labz[++num_labs]);
+    }
+
+    // for(int i = 1; i <= num_districts; i++){
+    //     cout << districts[i].id << " " << districts[i].name << " " << districts[i].lat << " ";
+    //     cout << districts[i].lon << " " << districts[i].samples <<  endl;
+    // }
+    // for(int i = 1; i <= num_labs; i++){
+    //     cout << labz[i].id << " " << labz[i].lat << " " << labz[i].lon << " " << labz[i].district << " ";
+    //     cout << labz[i].type << " " << labz[i].capacity << " " << labz[i].backlog << endl;
+    // }
+}
