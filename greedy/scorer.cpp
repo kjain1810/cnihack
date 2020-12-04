3f7ab1ff95f88c28f0878a15315cc8ad79cb4fd5
Arpan Dasgupta <arpandasgupta0@gmail.com>
2020-12-05 02:13:13 +0530

    Scorer half done

--- greedy/scorer.cpp
+++ greedy/scorer.cpp
@@ -93,6 +93,90 @@ pair<pair<int, int>, pair<int, int>> get_trans(string &line)
 
 int score()
 {
+    int totcost = 0;
+    set<int> overloaded_labs;
+    for (auto tr : transactions)
+    {
+        int dist = tr.first.second;
+        int lab = tr.second.first;
+        int amt = tr.second.second;
+        if (tr.first.first == 0)
+        {
+            // if (dist == 1)
+            //     cout << lab << " ++ ";
+            if (dist != labz[lab].district)
+            {
+                continue;
+            }
+            // if (dist == 1)
+            //     cout << lab << " ++ ";
+            if (districts[dist].samples < amt)
+            {
+                cout << "No samples left to send\n";
+                exit(0);
+            }
+            if (labz[lab].capacity >= amt)
+            {
+                int lab_cost = (labz[lab].type == 0) ? 800 : 1600;
+                labz[lab].capacity -= amt;
+                districts[dist].samples -= amt;
+                totcost += amt * lab_cost;
+            }
+            else if (labz[lab].capacity + 100 >= amt)
+            {
+                int lab_cost = (labz[lab].type == 0) ? 800 : 1600;
+                int init_amt = min(0, labz[lab].capacity);
+                int over_amt = amt - init_amt;
+                districts[dist].samples -= amt;
+                labz[lab].capacity -= amt;
+                totcost += lab_cost * init_amt + 5000 * over_amt;
+                overloaded_labs.insert(lab);
+            }
+            else
+            {
+                cout << "Lab cant overload further\n";
+                exit(0);
+            }
+        }
+        else
+        {
+            if (districts[dist].samples < amt)
+            {
+                cout << "No samples left to carry over\n";
+                exit(0);
+            }
+            districts[dist].samples -= amt;
+            totcost += 10000 * amt;
+        }
+    }
+    for (auto tr : transactions)
+    {
+        int dist = tr.first.second;
+        int lab = tr.second.first;
+        int amt = tr.second.second;
+        if (overloaded_labs.find(lab) != overloaded_labs.end())
+        {
+            if (labz[lab].district != dist)
+            {
+                cout << "Cant overload a lab from another district\n";
+                exit(0);
+            }
+        }
+        // if (dist == 1)
+        // {
+        //     cout << districts[1].samples << '\n';
+        // }
+    }
+    for (int i = 1; i <= num_districts; i++)
+    {
+        if (districts[i].samples > 0)
+        {
+            // cout << i << " " << districts[i].samples << '\n';
+            cout << "Unallocated swabs!\n";
+            exit(0);
+        }
+    }
+    return totcost;
 }
 
 signed main()
@@ -101,14 +185,13 @@ signed main()
     ifstream trans;
     trans.open("transactions.out"); // Change this
     string line;
-    getline(trans, line);
     while (getline(trans, line))
     {
         transactions.push_back(get_trans(line));
     }
     for (int i = 0; i < transactions.size(); i++)
     {
-        cout << transactions[i].first.first << " " << transactions[i].second.first << " " << transactions[i].second.second << " " << transactions[i].first.second << '\n';
+        cout << transactions[i].first.first << " " << transactions[i].first.second << " " << transactions[i].second.first << " " << transactions[i].second.second << '\n';
     }
 
     ifstream distin, labin;
@@ -128,14 +211,16 @@ signed main()
         get_lab(line, labz[++num_labs]);
     }
 
-    // for(int i = 1; i <= num_districts; i++){
+    // for (int i = 1; i <= num_districts; i++)
+    // {
     //     cout << districts[i].id << " " << districts[i].name << " " << districts[i].lat << " ";
-    //     cout << districts[i].lon << " " << districts[i].samples <<  endl;
+    //     cout << districts[i].lon << " " << districts[i].samples << endl;
     // }
-    // for(int i = 1; i <= num_labs; i++){
+    // for (int i = 1; i <= num_labs; i++)
+    // {
     //     cout << labz[i].id << " " << labz[i].lat << " " << labz[i].lon << " " << labz[i].district << " ";
     //     cout << labz[i].type << " " << labz[i].capacity << " " << labz[i].backlog << endl;
     // }
 
-    cout << score();
+    cout << "Your Score is = " << score() << '\n';
 }
