72b4ce4ec5c05b8a1c56566ff3ddfd43d6843419
Nikhil <nikhil.chandak@research.iiit.ac.in>
2020-12-14 11:33:37 +0530

    Added MIP (10 mins.) output

---
+++ Mallocators_Submission/MIP 10mins Output/constraint_checks_v2.py
@@ -0,0 +1,131 @@
+import pandas as pd 
+import numpy as np
+import math
+
+DEBUG_PRINTS = False
+
+
+COST_GOVT_TEST = 800
+COST_PVT_TEST = 1600
+COST_LAB_OVERLOAD = 5000
+COST_BACKLOG = 10000
+LAB_EXCESS_CAPACITY = 100
+LAB_MAX_RADIUS = 40
+RADIUS_TOLERANCE = 0
+OUTSIDE_DISTRICT_TRANSPORTATION_COST = 1000
+
+
+LAB_TYPE_GOVT = 0
+
+def backlog_same_distrct_constraint(df_solution):
+    #Check if backlogs are restricted to same district.
+    df_temp = df_solution[df_solution['transfer_type']==1].copy()
+    valid = df_temp['source'].equals(df_temp['destination'])
+    print("backlog_same_distrct_constraint: ", valid)
+    return valid
+
+def all_district_samples_accounted(df_districts,df_solution):
+    valid = True
+    for district_index in df_districts.index:
+        district_id = df_districts.loc[district_index,'district_id']
+        caseload = df_districts.loc[district_index,'samples']
+        allocated = df_solution[df_solution['source']==district_id]['samples_transferred'].sum()
+        valid = valid and (caseload == allocated)
+    print("all_district_samples_accounted: ", valid)
+    return valid
+
+def lab_capacity_accounted(df_labs,df_solution):
+    valid = True
+    for lab_index in df_labs.index.values:
+        lab_id = df_labs.loc[lab_index,'id']
+        lab_district_id = df_labs.loc[lab_index,'district_id']
+        lab_capacity = df_labs.loc[lab_index,'capacity']-df_labs.loc[lab_index,'backlogs']
+        outside_district_allocated = df_solution[(df_solution['transfer_type']==0) & (df_solution['destination']==lab_id) & (df_solution['source']!=lab_district_id)]['samples_transferred'].sum()
+        total_allocated = df_solution[(df_solution['transfer_type']==0) & (df_solution['destination']==lab_id)]['samples_transferred'].sum()
+        #print("lab_id: ", lab_id, ".lab_district_id", lab_district_id, ". lab_capacity: ",lab_capacity,". outside_district_allocated: ", outside_district_allocated,". total_allocated",total_allocated)
+        if(outside_district_allocated > lab_capacity):
+            valid = False
+            if(DEBUG_PRINTS) : print("outside_district_allocated exceeds capacity")
+        if(total_allocated > lab_capacity+LAB_EXCESS_CAPACITY):
+            valid = False
+            if(DEBUG_PRINTS) : print("Total allocation more than capacity +",LAB_EXCESS_CAPACITY)
+    print("Lab allocation constraints :", valid)
+
+    return valid
+
+def distance_haversine(lat1, lon1, lat2, lon2):
+    radius = 6371 # km
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return d
+
+def labs_within_radius(outside_district_transfer_lab_locations):
+    valid = True
+    if(len(outside_district_transfer_lab_locations) <= 1):
+        return valid
+    
+    centroid = np.mean(outside_district_transfer_lab_locations,axis=0)
+    for lab_location in outside_district_transfer_lab_locations:
+        for lab_location1 in outside_district_transfer_lab_locations:
+            distance_labs = distance_haversine(lab_location1[0],lab_location1[1], lab_location[0],lab_location[1])
+            if(DEBUG_PRINTS) : print("lab_location1",lab_location1, "lab_location:", lab_location,"distance_between_labs:",distance_labs)
+            if(distance_labs > (LAB_MAX_RADIUS+RADIUS_TOLERANCE)):
+                valid=False
+    return valid
+
+def outside_district_lab_distances(df_labs,df_solution):
+    valid = True
+    for district_id in df_solution[(df_solution['transfer_type']==0)]['source'].unique():
+        district_transfer_labs = df_solution[(df_solution['transfer_type']==0) & (df_solution['source']==district_id)]['destination'].unique()
+        outside_district_transfer_labs=[]
+        outside_district_transfer_lab_locations=[]
+        for lab in district_transfer_labs:
+            if df_labs[df_labs['id']==lab]['district_id'].values[0] !=district_id:
+                outside_district_transfer_labs.append(lab)
+                outside_district_transfer_lab_locations.append((df_labs[df_labs['id']==lab]['lat'].values[0],df_labs[df_labs['id']==lab]['lon'].values[0]))
+        valid = valid and labs_within_radius(outside_district_transfer_lab_locations)
+        if(DEBUG_PRINTS) : print("district_id: ",district_id, "outside_district_labs: ",outside_district_transfer_labs,"outside_district_transfer_lab_locations :",outside_district_transfer_lab_locations)
+        
+    print("outside_district_lab_distances : ",valid)
+    return valid
+
+def evaluate(lab_info_filename,district_info_filename,solution_filename):
+    df_labs = pd.read_csv(lab_info_filename).astype({"id":"int64","district_id":"int64","lab_type":"int64","capacity":"int64","backlogs":"int64"})
+    df_districts = pd.read_csv(district_info_filename).astype({"district_id":"int64","samples":"int64"})
+    df_solution = pd.read_csv(solution_filename).astype({"transfer_type":"int64","source":"int64","destination":"int64","samples_transferred":"int64"})
+    
+        
+    valid_solution = True
+    valid_solution = valid_solution and backlog_same_distrct_constraint(df_solution)
+    valid_solution = valid_solution and all_district_samples_accounted(df_districts,df_solution)
+    valid_solution = valid_solution and lab_capacity_accounted(df_labs,df_solution)
+    valid_solution = valid_solution and outside_district_lab_distances(df_labs,df_solution)    
+    
+    if(not valid_solution):
+        print("Constraints not met")
+        return
+    
+    
+    return valid_solution
+
+
+
+# input_dir = 'test_files/'
+input_dir = ''
+lab_file_prefix = 'lab_test_data_'
+district_file_prefix='district_test_data_'
+solution_file_prefix = 'test_data_'
+
+for count in range(1,6):
+    lab_input_filename=input_dir+lab_file_prefix+ "%03d" % count +".csv"
+    district_input_filename = input_dir+district_file_prefix+ "%03d" % count +".csv"
+    solution_filename = input_dir+solution_file_prefix+ "%03d" % count +"_solution.csv"
+    
+
+    valid_allocation = evaluate(lab_input_filename,district_input_filename,solution_filename)
+    print(solution_filename , ": Constraints Met : ", valid_allocation)
---
+++ Mallocators_Submission/MIP 10mins Output/scorer.py
@@ -0,0 +1,184 @@
+from geopy.distance import geodesic
+from math import radians, sin, cos, acos
+import pandas as pd
+from copy import deepcopy
+import math
+import sys 
+# pd.options.mode.chained_assignment = None
+kmR = 6373.0
+
+
+def toRadians(degree):
+    return (degree * math.pi) / 180
+
+
+def distance(lat1d, lon1d,  lat2d, lon2d):
+    # double lat1r, lon1r, lat2r, lon2r, u, v
+    lat1r = toRadians(lat1d)
+    lon1r = toRadians(lon1d)
+    lat2r = toRadians(lat2d)
+    lon2r = toRadians(lon2d)
+    u = math.sin((lat2r - lat1r) / 2)
+    v = math.sin((lon2r - lon1r) / 2)
+    return 2.0 * kmR * math.asin(math.sqrt(u * u + math.cos(lat1r) * math.cos(lat2r) * v * v))
+
+
+def dist2(lat1, lon1, lat2, lon2):
+    # R = 6373.0
+
+    # lat1a = math.radians(lat1)
+    # lon1a = math.radians(lon1)
+    # lat2a = math.radians(lat2)
+    # lon2a = math.radians(lon2)
+
+    # # dlon = lon2 - lon1
+
+    # # dlat = lat2 - lat1
+
+    # # a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+
+    # # c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    # # distance = R * c
+    # distance = 6371.01 * acos(sin(lat1a)*sin(lat2a) +
+    #                           cos(lat1a)*cos(lat2a)*cos(lon1a - lon2a))
+    # # print(distance)
+
+    distance = geodesic((lat1, lon1), (lat2, lon2)).km
+    return distance
+
+
+num = 2
+# transactions = pd.read_csv("solution_00"+str(num)+".csv")
+# districts = pd.read_csv("district_sample_data_00"+str(num)+".csv")
+# labs = pd.read_csv("lab_sample_data_00"+str(num)+".csv")
+transactions = pd.read_csv(sys.argv[3])
+districts = pd.read_csv(sys.argv[1])
+labs = pd.read_csv(sys.argv[2])
+
+labs['capacity'] -= labs['backlogs']
+
+totcost = 0
+num_districts = len(districts)
+num_labs = len(labs)
+print(num_districts, num_labs)
+overloaded_labs = set()
+# vector<vector<pair<pair<int, int>, pair<int, int>>>> district_transfer(num_districts + 1);
+district_transfer = [[] for i in range(num_districts+1)]
+
+# vector < int > lab_capacity(num_labs + 1, 0)
+lab_capacity = [0 for i in range(num_labs+1)]
+# vector < int > outside_transfer_to_lab(num_labs + 1, 0)
+outside_transfer_to_lab = [0 for i in range(num_labs+1)]
+
+for i in range(1, num_labs+1):
+    lab_capacity[i] = labs['capacity'][i-1]
+
+for i in range(len(transactions)):
+    dist = transactions['source'][i]-1
+    lab = transactions['destination'][i]-1
+    amt = transactions['samples_transferred'][i]
+    typ = transactions['transfer_type'][i]
+    if typ == 0:
+        if dist != labs['district_id'][lab]-1:
+            district_transfer[dist].append(deepcopy(transactions.iloc[i]))
+        if districts['samples'][dist] < amt:
+            print(districts['samples'][dist], amt, i)
+            print("No samples left to send\n")
+            exit(0)
+        if labs['capacity'][lab] >= amt:
+            lab_cost = 800 if labs['lab_type'][lab] == 0 else 1600
+            if totcost == 83780000:
+                print(lab_cost, "POOOO")
+            labs.loc[lab, 'capacity'] = labs['capacity'][lab] - amt
+            districts.loc[dist, 'samples'] = districts['samples'][dist] - amt
+            totcost += amt * lab_cost
+        elif labs['capacity'][lab] + 100 >= amt:
+            lab_cost = 800 if labs['lab_type'][lab] == 0 else 1600
+            init_amt = max(0, labs['capacity'][lab])
+            over_amt = amt - init_amt
+            districts.loc[dist, 'samples'] = districts['samples'][dist] - amt
+            labs.loc[lab, 'capacity'] = labs['capacity'][lab] - amt
+            totcost += lab_cost * init_amt + (5000 + lab_cost) * over_amt
+            overloaded_labs.add(lab)
+        else:
+            print("Lab cant overload further\n")
+            exit(0)
+    else:
+        if districts['samples'][dist] < amt:
+            print("No samples left to carry over\n")
+            exit(0)
+        districts.loc[dist, 'samples'] = districts['samples'][dist] - amt
+        totcost += 10000 * amt
+    print(totcost)
+
+print(totcost)
+
+for i in range(num_districts):
+    cnt = 0
+    lat = 0
+    lon = 0
+    for tr in district_transfer[i]:
+        dist = tr['source']-1
+        lab = tr['destination']-1
+        amt = tr['samples_transferred']
+        cnt += 1
+        lat += labs['lat'][lab]
+        lon += labs['lon'][lab]
+        for tr2 in district_transfer[i]:
+            lab2 = tr2['destination']-1
+            if (dist2(labs['lat'][lab], labs['lon'][lab], labs['lat'][lab2], labs['lon'][lab2]) > 40):
+                print("District " + str(dist) + " sending samples to labs " +
+                      str(lab) + " and " + str(lab2) + " which are more than 40km apart\n")
+                exit(0)
+    if cnt == 0:
+        continue
+    lat = lat / cnt
+    lon = lon / cnt
+    dist = dist2(districts['lat'][i], districts['lon'][i], lat, lon)
+    totcost += 1000 * dist
+
+print(totcost)
+# for (auto tr: transactions)
+# {
+#     int dist = tr.first.second
+#     int lab = tr.second.first
+#     int amt = tr.second.second
+#     // if (overloaded_labs.find(lab) != overloaded_labs.end())
+#     // {
+#         // if (labz[lab].district != dist)
+#         // {
+#             // cout << "Cant overload a lab from another district\n"
+#             // exit(0)
+#             // }
+#         //}
+#     if (tr.first.first != 0)
+#     continue
+#     if (dist != labz[lab].district)
+#     {
+#         outside_transfer_to_lab[lab] += amt
+#         if (outside_transfer_to_lab[lab] > lab_capacity[lab])
+#         {
+#             cout << "Cant overload a lab from another district\n"
+#             cout << "Lab " << lab << " overloaded by outside districts\n"
+#             cout << "Lab capacity - " << lab_capacity[lab] << '\n'
+#             exit(0)
+#         }
+#     }
+#     // if (dist == 1)
+#     // {
+#         // cout << districts[1].samples << '\n'
+#         //}
+# }
+
+# for (int i=1
+#      i <= num_districts
+#      i++)
+# {
+#     if (districts[i].samples > 0)
+#     {
+#         // cout << i << " " << districts[i].samples << '\n'
+#         cout << "Unallocated swabs!\n"
+#         exit(0)
+#     }
+# }
+# return totcost
